88041f047c49dd6c012a9c0466b1ffe9a776686d
Max <canterom886@gmail.com>
2024-07-31 14:27:34 -0400

    feat: add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'tw-',
-  content: [
-    './**/*.liquid',
-    './frontend/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
+	prefix: "tw-",
+	content: ["./**/*.liquid", "./frontend/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				tbgOrange: "#af431f",
+				tbgYellow: "#f2b65b",
+			},
+		},
+	},
+	plugins: [],
+};
